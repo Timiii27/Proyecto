f076e586f204cb18f00e4afaca2465fbdd7fc355
Timiii27 <tihomir27402@gmail.com>
2022-01-17 19:55:50 +0100

    Actualizar 2022

--- xml/guardar.php
+++ xml/guardar.php
@@ -4,14 +4,14 @@ $cache_drivers = 'drivers.xml.cache';
 // generate the cache version if it doesn't exist or it's too old!
 $ageInSeconds_1 = 3600; // one hour
 if(!file_exists($cache_drivers) || filemtime($cache_drivers) > time() + $ageInSeconds_1) {
-  $contents = file_get_contents('http://ergast.com/api/f1/current/driverStandings');
+  $contents = file_get_contents('http://ergast.com/api/f1/2022/driverStandings');
   file_put_contents($cache_drivers, $contents);
 }
 $cache_constructors = 'constructors.xml.cache';
 // generate the cache version if it doesn't exist or it's too old!
 $ageInSeconds_2 = 3600; // one hour
 if(!file_exists($cache_constructors) || filemtime($cache_constructors) > time() + $ageInSeconds_2) {
-  $contents = file_get_contents('http://ergast.com/api/f1/current/constructorStandings');
+  $contents = file_get_contents('http://ergast.com/api/f1/2022/constructorStandings');
   file_put_contents($cache_constructors, $contents);
 }
 
